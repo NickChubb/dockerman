57dbc141c635fd41b7dcd1a7aa46efac6432c330
Nick Chubb <nick.r.chubb@gmail.com>
2021-07-28 12:59:22 -0700

    Update system info

--- src/components/SystemInfo.js
+++ src/components/SystemInfo.js
@@ -1,17 +1,25 @@
-import { fetchSystemInfo, fetchDockerVersion } from './api/system.js';
+import { fetchSystemInfo, fetchDockerVersion, fetchInfo } from './api/system.js';
 import { formatBytes } from './api/helpers.js';
 import { useState, useEffect } from 'react';
 
 const SystemInfo = () => {
 
     const [info, setInfo] = useState([]);
+    const [dockerInfo, setDockerInfo] = useState([]);
     const [version, setVersion] = useState([]);
 
     useEffect(() => {
-        const getSystemInfo = async () => {
-            const systemInfo = await fetchSystemInfo();
+
+        const getInfo = async () => {
+            const systemInfo = await fetchInfo();
             console.log(systemInfo);
             setInfo(systemInfo);
+        }
+
+        const getDockerInfo = async () => {
+            const dockerInfo = await fetchSystemInfo();
+            console.log(dockerInfo);
+            setDockerInfo(dockerInfo);
         }
 
         const getDockerVersion = async () => {
@@ -19,13 +27,14 @@ const SystemInfo = () => {
             setVersion(dockerVersion);
         }
 
-        getSystemInfo();
+        getInfo();
+        getDockerInfo();
         getDockerVersion();
     }, []); 
 
     return (
         <span>
-            <strong>Docker Size:</strong> {formatBytes(info.LayersSize)} | <strong>Docker Version:</strong> {version.Version}
+            <strong>Docker Size:</strong> {formatBytes(dockerInfo.LayersSize)} | <strong>Docker Version:</strong> {version.Version} | 
         </span>
     )
 }
--- src/components/api/system.js
+++ src/components/api/system.js
@@ -1,25 +1,39 @@
 import { homepage } from '../../../package.json';
 
+// Fetch os info from API and return it
+export const fetchInfo = async (debug=false) => {
+
+    let res = await fetch(`${homepage}/api/sys/info`);
+    let data = await res.json();
+
+    if (debug) console.log(data);
+    return data;
+}
+
 // Fetch system info (df) from API and return it
-export const fetchSystemInfo = async () => {
+export const fetchSystemInfo = async (debug=false) => {
     let res = await fetch(`${homepage}/api/sys/systemInfo`);
     let data = await res.json();
-    // console.log(data);
+
+    if (debug) console.log(data);
     return data;
 }
 
 // Fetch docker version from API and return it
-export const fetchDockerVersion = async () => {
+export const fetchDockerVersion = async (debug=false) => {
     let res = await fetch(`${homepage}/api/sys/dockerVersion`);
     let data = await res.json();
-    // console.log(data);
+
+    if (debug) console.log(data);
     return data;
 }
 
 // Fetch config JSON file from the server
-export const fetchConfig = async () => {
+export const fetchConfig = async (debug=false) => {
     let res = await fetch(`${homepage}/api/sys/config`);
     let data = await res.json();
+
+    if (debug) console.log(data);
     return data;
 }
 
@@ -37,3 +51,4 @@ export const updateConfig = async (config) => {
     let data = await res.json();
     return data;
 }
+
