fe423c0d6a659496641cd2d7bc6e34c6744ea51e
Nick Chubb <nick.r.chubb@gmail.com>
2021-08-05 10:43:37 -0700

    Update config page css.

--- src/components/screens/config/TextParameter.js
+++ src/components/screens/config/TextParameter.js
@@ -3,18 +3,20 @@ import { Form, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 const TextParameter = ({ setConfig, param, value }) => {
 
     return (
-        <InputGroup className="form-parameter">
-            <span> {param} </span>
-            <FormControl
-	    	    className="form-entry"
-                    id={param}
-                    name={param}
-                    defaultValue={value}
-                    onChange={e => {
-                        
-                    }} 
-                /> 
-        </InputGroup>
+        <div className="form-parameter">
+            <span className="form-title"> {param} </span>
+            <InputGroup className="form-entry">
+                <FormControl
+                        className="form-entry-text"
+                        id={param}
+                        name={param}
+                        defaultValue={value}
+                        onChange={e => {
+                            
+                        }} 
+                    /> 
+            </InputGroup>
+        </div>
     )
 }
 
